496e635f042c49a4737430b63aef3e9476d39aaf
Maximilien Cura <70043540+max-cura@users.noreply.github.com>
2026-01-18 17:35:03 -0800

    Clean up and add supporting docs (minus armv8a)

--- lace/src/main.rs
+++ lace/src/main.rs
@@ -1,5 +1,10 @@
 #![no_std]
 #![no_main]
+#![feature(stdarch_arm_barrier)]
+
+use core::arch::aarch64::__dsb;
+
+use tock_registers::Write as _;
 
 core::arch::global_asm!(
     r#"
@@ -65,17 +70,156 @@ pub extern "C" fn cpu0_start2() -> ! {
 }
 
 pub extern "C" fn cpu0_start3() -> ! {
-    let gpset1 = 0xfe20_0020 as *mut u32;
-    let gpclr1 = 0xfe20_002c as *mut u32;
-    loop {
-        unsafe { gpset1.write_volatile(0xffff_ffff) }
-        for _ in 0..0x100_0000 {
-            unsafe { core::arch::asm!("") }
-        }
-        unsafe { gpclr1.write_volatile(0xffff_ffff) }
-        for _ in 0..0x100_0000 {
-            unsafe { core::arch::asm!("") }
-        }
+    // let gpset1 = 0xfe20_0020 as *mut u32;
+    // let gpclr1 = 0xfe20_002c as *mut u32;
+    // loop {
+    //     unsafe { gpset1.write_volatile(0xffff_ffff) }
+    //     for _ in 0..0x100_0000 {
+    //         unsafe { core::arch::asm!("") }
+    //     }
+    //     unsafe { gpclr1.write_volatile(0xffff_ffff) }
+    //     for _ in 0..0x100_0000 {
+    //         unsafe { core::arch::asm!("") }
+    //     }
+    // }
+    let gpfsel1 = 0xfe20_0004 as *mut u32;
+    unsafe { gpfsel1.write_volatile(gpfsel1.read_volatile() & !0o770000 | 0o440000) };
+
+    unsafe { __dsb(core::arch::aarch64::SY) }
+
+    // TODO: set up UART
+    // TODO: print with UART
+
+    // default: UARTCLK @ 48MHz, so baud rate is at most 3MHz by default
+    // thus, we'll need to take the clock rate up to 96MHz
+
+    loop {}
+}
+
+fn summon_uart(no: u8) -> Option<HwPL011> {
+    if no == 1 || no >= 6 {
+        None
+    } else {
+        Some(HwPL011::from_addr(0xfe201000 + 0x200 * usize::from(no)))
+    }
+}
+
+tock_registers::register_bitfields![u32,
+    pub Data [
+        OE OFFSET(11) NUMBITS(1) [],
+        BE OFFSET(10) NUMBITS(1) [],
+        PE OFFSET(9) NUMBITS(1) [],
+        FE OFFSET(8) NUMBITS(1) [],
+        DATA OFFSET(0) NUMBITS(8) [],
+    ],
+    pub ReceiveStatusErrorClear [
+        OE 3,
+        BE 2,
+        PE 1,
+        FE 0,
+    ],
+    pub Flag [
+        RI 8,
+        TXFE 7,
+        RXFF 6,
+        TXFF 5,
+        RXFE 4,
+        BUSY 3,
+        CTS 0,
+    ],
+    // Baud rate divisor calculation:
+    //
+    //  BAUDDIV = FUARTCLK / (16 * Baud Rate)
+    pub IntegerBaudRateDivisor [
+        IBRD OFFSET(0) NUMBITS(16) [],
+    ],
+    pub FractionalBaudRateDivisor [
+        FBRD OFFSET(0) NUMBITS(6) [],
+    ],
+    pub LineControl [
+        SPS OFFSET(7) NUMBITS(1) [],
+        WLEN OFFSET(5) NUMBITS(2) [
+            _8 = 0b11,
+            _7 = 0b10,
+            _6 = 0b01,
+            _5 = 0b00,
+        ],
+        FEN OFFSET(4) NUMBITS(1) [],
+        STP2 OFFSET(3) NUMBITS(1) [],
+        EPS OFFSET(2) NUMBITS(1) [
+            Odd = 0,
+            Even = 1,
+        ],
+        PEN OFFSET(1) NUMBITS(1) [],
+        BRK OFFSET(0) NUMBITS(1) [],
+    ],
+    pub Control [
+        CTSEN 15,
+        RTSEN 14,
+        RTS 11,
+        RXE 9,
+        TXE 8,
+        LBE 7,
+        UARTEN 0,
+    ],
+    pub InterruptFifoLevelSelect [
+        RXIFPSEL OFFSET(9) NUMBITS(3) [],
+        TXIFPSEL OFFSET(6) NUMBITS(3) [],
+        RXIFLSEL OFFSET(3) NUMBITS(3) [],
+        TXIFLSEL OFFSET(0) NUMBITS(3) [],
+    ],
+    pub InterruptMaskSetClear [
+        OEIM 10,
+        BEIM 9,
+        PEIM 8,
+        FEIM 7,
+        RTIM 6,
+        TXIM 5,
+        RXIM 4,
+        CTSMIM 1,
+    ],
+    pub InterruptStatus [
+        OERIS 10,
+        BERIS 9,
+        PERIS 8,
+        FERIS 7,
+        RTRIS 6,
+        TXRIS 5,
+        RXRIS 4,
+        CTSRMIS 1,
+    ],
+    pub InterruptClear [
+        OEIC 10,
+        BEIC 9,
+        PEIC 8,
+        FEIC 7,
+        RTIC 6,
+        TXIC 5,
+        RXIC 4,
+        CTSMIC 1,
+    ],
+    pub DmaControl [
+        DMAONERR 2,
+        TXDMAE 1,
+        RXDMAE 0,
+    ],
+];
+tock_registers::peripheral! {
+    #[real(HwPL011)]
+    pub PL011 {
+        0x00 => dr : Data::Register { Read, Write },
+        0x04 => rsrecr : ReceiveStatusErrorClear::Register { Read, Write },
+        0x18 => fr : Flag::Register { Read },
+        0x24 => ibrd : IntegerBaudRateDivisor::Register { Read, Write },
+        0x28 => fbrd : FractionalBaudRateDivisor::Register { Read, Write },
+        0x2c => lcrh : LineControl::Register { Read, Write },
+        0x30 => cr : Control::Register { Read, Write },
+        0x34 => ifls : InterruptFifoLevelSelect::Register { Read, Write },
+        0x38 => imsc : InterruptMaskSetClear::Register { Read, Write },
+        0x3c => ris : InterruptStatus::Register { Read },
+        0x40 => mis : InterruptStatus::Register { Read },
+        0x44 => icr : InterruptClear::Register { Write },
+        0x48 => dmacr : DmaControl::Register { Read, Write },
     }
 }
 
--- rev/Hardware.md
+++ rev/Hardware.md
@@ -15,16 +15,47 @@ Documents:
 
 ### Gross Memory Layout
 
-0x0_0000_0000 : 0x0_4000_0000-%gpu_mem   ARM DRAM 0
-0x0_4000_0000 : ?                        ARM DRAM 1
+```
+0x0_0000_0000 : 0x0_4000_0000-$gpu_mem   ARM DRAM 0
+0x0_4000_0000 : (purchase option)        ARM DRAM 1
 0x6_0000_0000 : 0x6_4000_0000            PCI
+```
 
 In the middle is the section of memory reserved by the VC.
+Note that this is _not_ the peripherals block.
+
+The location of the peripherals depends on configuration: there is a configuration option,
+`arm_peri_high`, that controls whether the Broadcom/ARM peripherals are mapped in at `FC00_0000`
+or `4_7C00_0000`. The default is `FC00_0000`, for 32-bit compat reasons, but `4_7C00_0000` allows
+all of DRAM to be mapped on devices with 4GiB of DRAM (while LPAE would allow for its use on 32-bit
+kernels, it would apparently complicate the early stage bootloader, so the Pi folks leave it be).
+
 
 ### Memory Hierarchy
 
- BCM2711 - L2
+```
+  DRAM
     |
-Cortex-A72 - L2
+ BCM2711 | L2
+    |
+Cortex-A72 | L2
              |
              L1 L1 L1 L1
+```
+As far as I can tell, this is the way the cache hierarchy on the pi4 works: L1s per core on the A72
+and an L2 on the A72, which then goes to the VC's L2, and subsequently the DRAM.
+
+> [!TODO]
+> Haven't actually figured out the sizes of the different pieces yet.
+
+In this case, the VC's L2 is almost a kind of L3?
+
+### Clock Hierarchy
+
+This is mostly extrapolated from reverse engineering on prior SoCs in the 2708-series, but it seems
+like clocks live in the CM block at `E10_1000:E10_2000`.
+These are the lower-level clocks that feed off the PLLs, however, and only have prescaler
+functionality.
+
+It is _believed_ that the PLLs are controlled by the A2W region at `E10_2000`, and the PLLx
+multiplier registers are known, at the very least.
